3ccf7543ac42e48eecbe939a6bd6b3a138b09e0c
tinix84 <tinix84@gmail.com>
2019-03-19 10:38:52 +0100

    MOD: reorganize file structure

---
+++ importfile_Eoff.m
@@ -0,0 +1,53 @@
+function Eoff = importfile_Eoff(filename, startRow, endRow)
+%% Initialize variables.
+delimiter = {'\t',' '};
+
+%% Format for each line of text:
+%   column1: double (%f)
+%	column2: double (%f)
+%   column3: double (%f)
+%	column4: double (%f)
+%   column5: double (%f)
+%	column6: double (%f)
+%   column7: double (%f)
+%	column8: double (%f)
+%   column9: double (%f)
+%	column10: double (%f)
+%   column11: double (%f)
+%	column12: double (%f)
+%   column13: double (%f)
+%	column14: double (%f)
+% For more information, see the TEXTSCAN documentation.
+formatSpec = '%f%f%f%f%f%f%f%f%f%f%f%f%f%f%[^\n\r]';
+
+%% Open the text file.
+fileID = fopen(filename,'r');
+
+%% Read columns of data according to the format.
+% This call is based on the structure of the file used to generate this
+% code. If an error occurs for a different file, try regenerating the code
+% from the Import Tool.
+textscan(fileID, '%[^\n\r]', startRow(1)-1, 'WhiteSpace', '', 'ReturnOnError', false);
+dataArray = textscan(fileID, formatSpec, endRow(1)-startRow(1)+1, 'Delimiter', delimiter, 'MultipleDelimsAsOne', true, 'TextType', 'string', 'EmptyValue', NaN, 'ReturnOnError', false, 'EndOfLine', '\r\n');
+for block=2:length(startRow)
+    frewind(fileID);
+    textscan(fileID, '%[^\n\r]', startRow(block)-1, 'WhiteSpace', '', 'ReturnOnError', false);
+    dataArrayBlock = textscan(fileID, formatSpec, endRow(block)-startRow(block)+1, 'Delimiter', delimiter, 'MultipleDelimsAsOne', true, 'TextType', 'string', 'EmptyValue', NaN, 'ReturnOnError', false, 'EndOfLine', '\r\n');
+    for col=1:length(dataArray)
+        dataArray{col} = [dataArray{col};dataArrayBlock{col}];
+    end
+end
+
+%% Close the text file.
+fclose(fileID);
+
+%% Post processing for unimportable data.
+% No unimportable data rules were applied during the import, so no post
+% processing code is included. To generate code which works for
+% unimportable data, select unimportable cells in a file and regenerate the
+% script.
+
+%% Create output variable
+Eoff_all = [dataArray{1:end-1}];
+Eoff=Eoff_all(:,2);
+end
